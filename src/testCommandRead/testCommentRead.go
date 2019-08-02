2bebf14cb7377d751e11bfeadc2b2025029ec21b
Wendy Kwan <poofiemoosie@gmail.com>
2019-08-01 17:38:47 -0700

    test squash merge #1

--- src/testCommandRead/testCommentRead.go
+++ src/testCommandRead/testCommentRead.go
@@ -21,11 +21,6 @@ func handleWebhook(w http.ResponseWriter, r *http.Request) {
 func main() {
 	log.Println("server started")
 	log.Println("squash check #1")
-	log.Println("squash check #2")
-	log.Println("squash check #3")
-	log.Println("test sync merge #1")
-	log.Println("test sync merge #2")
-	log.Println("test sync merge #3")
 	http.HandleFunc("/webhook", handleWebhook)
 	log.Fatal(http.ListenAndServe(":12345", nil))
 }
