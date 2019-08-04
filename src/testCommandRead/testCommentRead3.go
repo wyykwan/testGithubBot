5b154218e22d865c5a9b3a3e27c5f612a8b2b63d
Wendy Kwan <poofiemoosie@gmail.com>
2019-08-04 03:35:14 -0700

    add testFEATUREMERGE #1

---
+++ src/testCommandRead/testCommentRead3.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"os"
+)
+
+func handleWebhook3(w http.ResponseWriter, r *http.Request) {
+	fmt.Printf("headers: %v\n\n", r.Header)
+
+	_, err := io.Copy(os.Stdout, r.Body)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+}
+
+func main() {
+	log.Println("server started")
+	log.Println("feature merge check #1")
+	http.HandleFunc("/webhook", handleWebhook3)
+	log.Fatal(http.ListenAndServe(":12345", nil))
+}
