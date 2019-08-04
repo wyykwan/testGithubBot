fcec1b6af195256902012299f8f9da5aa93e0807
Wendy Kwan <poofiemoosie@gmail.com>
2019-08-04 03:35:46 -0700

    add testFEATUREMERGE #2

--- src/testCommandRead/testCommentRead3.go
+++ src/testCommandRead/testCommentRead3.go
@@ -21,6 +21,7 @@ func handleWebhook3(w http.ResponseWriter, r *http.Request) {
 func main() {
 	log.Println("server started")
 	log.Println("feature merge check #1")
+	log.Println("feature merge check #2")
 	http.HandleFunc("/webhook", handleWebhook3)
 	log.Fatal(http.ListenAndServe(":12345", nil))
 }
