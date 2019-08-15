825daacbf712ada022129ac7d12bc04471df2927
wyykwan <poofiemoosie@gmail.com>
2019-08-15 12:00:12 -0700

    Add test squashhhh
    pr-link: wyykwan/testGithubBot#18
    change-id: cid-60e520923020656fc240de8e821c231d057b86ca

---
+++ src/testCommandRead/blehh.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"os"
+)
+
+func handleWebhook5(w http.ResponseWriter, r *http.Request) {
+	fmt.Printf("headers: %v\n\n", r.Header)
+
+	_, err := io.Copy(os.Stdout, r.Body)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+}
+
+func main() {
+	log.Println("server started")
+	log.Println("squash #1")
+	log.Println("squash #2")
+	http.HandleFunc("/webhook", handleWebhook5)
+	log.Fatal(http.ListenAndServe(":12345", nil))
+}
