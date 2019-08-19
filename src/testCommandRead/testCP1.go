9b0a3395a7b8b0aca21d0649a9dee9de78a1e84d
wyykwan <poofiemoosie@gmail.com>
2019-08-19 16:44:02 -0700

    Add new file
    Auto cherry-pick of existing commit.
    orig-pr: wyykwan/testGithubBot#56
    orig-commit: wyykwan/testGithubBot@a9c94c38d94328ad753c78ce47d64b108c5567f8
    orig-commit-author: wyykwan <poofiemoosie@gmail.com>
    
    pr-link: wyykwan/testGithubBot#56
    change-id: cid-36def65011743e20b8a5470da6a468ad29519c92

--- src/testCommandRead/testCP1.go
+++ src/testCommandRead/testCP1.go
@@ -8,7 +8,7 @@ import (
 	"os"
 )
 
-func kwaaaan(w http.ResponseWriter, r *http.Request) {
+func kwaan(w http.ResponseWriter, r *http.Request) {
 	fmt.Printf("headers: %v\n\n", r.Header)
 
 	_, err := io.Copy(os.Stdout, r.Body)
@@ -20,6 +20,6 @@ func kwaaaan(w http.ResponseWriter, r *http.Request) {
 
 func main() {
 	log.Println("server started")
-	http.HandleFunc("/webhook", kwaaaan)
+	http.HandleFunc("/webhook", kwaan)
 	log.Fatal(http.ListenAndServe(":12345", nil))
 }
