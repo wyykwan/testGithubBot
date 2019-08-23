c8ef3ee322b035e7e99bc73e8cd3a5975fa62a56
wyykwan <poofiemoosie@gmail.com>
2019-08-23 14:11:58 -0700

    Add testtt
    pr-link: wyykwan/testGithubBot#81
    change-id: cid-0eec1a7990735242aeeb74f85216f0166b17e4cb

---
+++ src/testCommandRead/yikes.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"os"
+)
+
+func thorrr (w http.ResponseWriter, r *http.Request) {
+	fmt.Printf("headers: %v\n\n", r.Header)
+
+	_, err := io.Copy(os.Stdout, r.Body)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+}
+
+func main() {
+	log.Println("server starteddddd")
+	http.HandleFunc("/webhook", thorrr)
+	log.Fatal(http.ListenAndServe(":12345", nil))
+}
